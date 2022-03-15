089566b0e54774bbcc204b93c661b5b5078d4d70
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-15 17:07:17 +0200

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.c"
+#include "main.h"
+
+/**
+* main - prints _putchar
+* Return: 0
+*/
+
+int main(void)
+{
+	char str[8] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int i = 0;
+
+	while (i < 8)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
