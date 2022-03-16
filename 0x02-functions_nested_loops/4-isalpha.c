66c05de03cc5e1fcdbe9b37ac73af987f0de7662
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-16 09:54:45 +0200

    check if small case letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_isalpha- checks if char us lower case
+*@c: integer c to check
+* Return: int
+*/
+
+int _isalpha(int c)
+{
+        if (c >= 97 && c >= 97)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
+
