e59c250acafd27b75dc657b667fcbcba462339c3
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-16 10:51:08 +0200

    return last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*print_last_digit- checks if char us lower case
+*@n: integer c to check
+* Return: int
+*/
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
