f7642953d5f90c373a16b30c3b7cf91ae683eedd
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-16 10:42:40 +0200

    return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_abs- checks if char us lower case
+*@n: integer c to check
+* Return: int
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n / (-1));
+	}
+	return (n);
+}
+
