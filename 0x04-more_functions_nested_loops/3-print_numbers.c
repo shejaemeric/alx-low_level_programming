94e0b7127f5b2bc84ee14250590635fef85761c6
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-17 10:16:37 +0200

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+void print_numbers(void);
+
+#include "main.h"
+
+/**
+*print_numbers(void)- checks if char is a digit case
+* Return: void
+*/
+
+print_numbers(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		_putchar(a);
+		a++;
+	}
+}
