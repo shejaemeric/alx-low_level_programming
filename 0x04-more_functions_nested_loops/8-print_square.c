569d0ec6ce2c45b1d0a9866c621cb70750df9321
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-17 11:14:51 +0200

    print a square of # based on input

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_square- prints a square of #
+*@size:number of \ to print
+* Return: void
+*/
+
+void print_square(int size)
+{
+	int c = 1;
+	int b;
+
+	while (c <= size)
+	{
+		b = 1;
+		while (b <= size)
+		{
+			_putchar('#');
+			b++;
+		}
+		_putchar('\n');
+		c++;
+	}
+	_putchar('\n');
+}
