faf365672bfae506da73c432756c9abee0b26ef7
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-17 10:20:07 +0200

    print 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,7 +5,7 @@
 * Return: void
 */
 
-print_numbers(void)
+void print_numbers(void)
 {
 	int a = 48;
 
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,7 +7,7 @@ void print_numbers(void);
 * Return: void
 */
 
-print_numbers(void)
+void print_most_numbers(void);
 {
 	int a = 48;
 
