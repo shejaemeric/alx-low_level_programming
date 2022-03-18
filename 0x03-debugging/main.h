ad819a1fac78c22005bce68028b428d11b9b5bf0
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-18 13:31:48 +0200

    updates

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+
 void positive_or_negative(int i);
-void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
 #endif
+
