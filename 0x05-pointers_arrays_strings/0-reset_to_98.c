980f5cfb66822b95384d11617a03a090cfdf2818
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-21 10:16:56 +0200

    reset variable to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset pointer value to 98
+ * @n: month in number format
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (o);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/* the entire header file file */
+
+void reset_to_98(int *n);
+
+#endif
