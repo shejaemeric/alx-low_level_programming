0ebe4165a9051b8538df49aac65fe6e8e4076a49
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-21 10:19:40 +0200

    reset variable to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/*
  * reset_to_98 - reset pointer value to 98
  * @n: month in number format
  * Return: void
@@ -9,5 +9,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return (o);
 }
