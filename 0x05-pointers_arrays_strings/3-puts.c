d95bba8265739ba664b6c6f19348bacf19c76960
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-21 10:48:04 +0200

    print a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - reset pointer value to 98
+ * @str: month in number format
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i >= 0; i++)
+	{
+		if (s[i] == '\0')
+			break;
+		_putchar(s[i]);
+	}
+}
