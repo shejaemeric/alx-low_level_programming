35b10a0e3d27010d3424ecaa92ae4db82897baf6
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-21 11:10:29 +0200

    print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,19 +9,17 @@
 void print_rev(char *s)
 {
 	int i, count = 0;
-	char arr[];
 
 	for (i = 0; i >= 0; i++)
 	{
-		if (str[i] == '\0')
+		if (s[i] == '\0')
 			break;
-		arr[i] = str[i];
-		_putchar(str[i]);
+		_putchar(s[i]);
 		count++;
 	}
 	for (i = count - 1; i >= 0; i--)
 	{
-		_putchar(arr[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
