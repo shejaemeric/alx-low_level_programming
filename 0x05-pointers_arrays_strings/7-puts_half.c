36f8adf00f228c0e9c2156c586f66fe2c6fdafdf
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-21 13:50:27 +0200

    print half of characters

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,18 +10,18 @@ void puts_half(char *str)
 
 	for (i = 0; i >= 0; i++)
 	{
-		if (s[i] == '\0')
+		if (str[i] == '\0')
 			break;
 		count++;
 	}
 	if (count % 2 == 0)
-		n = length_of_the_string / 2;
+		n = count / 2;
 	else
-		n = (length_of_the_string - 1) / 2;
+		n = (count - 1) / 2;
 
 	for (i = n - 1; i <= count - 1; i++)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
