e617ec3c9c979e209eec9caacf5e289872b91591
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-03-21 11:46:37 +0200

    a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: arg1
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; i >= 0; i++)
+	{
+		if (s[i] == '\0')
+			break;
+		count++;
+	}
+
+	int arr[count];
+
+	for (i = count - 1; i >= 0; i--)
+	{
+		arr[i] == s[count - i]
+	}
+
+	for (i = 0; i <= count - 1; i++)
+	{
+		s[i] == arr[i];
+	}
+	_putchar('\n');
+}
