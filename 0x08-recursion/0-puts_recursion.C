23b40066e5f35690fce2d16eab3524c282005083
shejaemeric <shejaemeric>
2022-03-30 19:50:41 +0000

    recursion printing

---
+++ 0x08-recursion/0-puts_recursion.C
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_memset - function........
+*@s: arg1
+*Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+}
