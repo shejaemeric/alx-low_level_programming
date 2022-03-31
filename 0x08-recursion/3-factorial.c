0786d03742ff3c7093f23859f2fb5b24fe7786a6
shejaemeric <shejaemeric>
2022-03-31 06:19:28 +0000

    find factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,12 @@
 *Return: void
 */
 
-int factorial(int n){
+int factorial(int n)
+{
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
 	else
-		return (n * factorial(n-1));
+		return (n * factorial(n -1));
 }
