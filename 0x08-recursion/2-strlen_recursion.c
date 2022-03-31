d71e7aa138229be98e4d52a51576522693c5ee6a
shejaemeric <shejaemeric>
2022-03-31 06:10:23 +0000

    return length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strlen_recursion - function........
+*@s: arg1
+*Return: void
+*/
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s == '\0')
+	{
+		return (count);
+	}
+	count++;
+	return (count + _strlen_recursion(s + 1));
+}
