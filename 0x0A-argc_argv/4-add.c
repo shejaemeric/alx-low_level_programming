501bfe9ae5d16e023fbfa6749aec16338e1d140f
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-04-03 07:42:06 +0300

    last tasks

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,52 +10,20 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int main(int argc, char **argv)
+*int main(int argc, char *argv[])
 {
-	int i;
-	int total = 0;
-
-	/* if no other arguments are passed */
-	if (argc < 2)
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
+	int i, num, res = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		/* check if the argument is a valid integer */
-		if (is_valid_int(argv[i]))
-		{
-			total += atoi(argv[i]);
-		}
-		else
+		num = atoi(argv[i]);
+		if (!num)
 		{
 			printf("Error\n");
 			return (1);
 		}
+		res += num;
 	}
-
-	printf("%d\n", total);
-
+	printf("%d\n", res);
 	return (0);
-}
-
-/**
- * is_valid_int - checks if a string is a valid integer
- * @s: string
- *
- * Return: 1 is s is a valid integer. 0 otherwise
- */
-int is_valid_int(char *s)
-{
-	while (*s)
-	{
-		if (*s < '0' || *s > '9')
-			return (0);
-
-		s++;
-	}
-
-	return (1);
 }
