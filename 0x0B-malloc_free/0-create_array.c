dffc50158e64e487ae3237fc1587043fda75d962
shejaemeric <shejaemeric>
2022-04-06 10:33:32 +0000

    create array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *create_array -function that creates an array of chars, and initializes it with a specific cha
+ *@size:size
+ *@c:char
+ * Return: Always 0.
+ */
+
+char *create_array(unsigned int size, char c){
+	char *str;
+
+	if (size == 0)
+	{
+		return (null);
+	}
+	str = malloc(sizeof(c) * size);
+	while (size > 0)
+	{
+		str[size] = c;
+		size--;
+	}
+	return (str);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/* the entire header file file */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *_strdup(char *str);
+int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height);
+#endif
