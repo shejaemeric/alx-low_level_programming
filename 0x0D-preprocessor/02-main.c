a4aa63375e6ae37ce41a3b3a8dfc8ec63cbb7e98
shejaemeric <shejaemeric>
2022-04-11 19:30:58 +0000

    last task

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,35 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * main - A function that creates an array of chars,
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
-
-	char s[] = __FILE__;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
 }
-
-
-
