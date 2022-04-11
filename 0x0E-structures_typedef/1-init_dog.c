9bd3468af35f9e901326cf743818f2d91862f0d5
shejaemeric <shejaemeric>
2022-04-11 18:57:05 +0000

    initialize structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "dog.h"
+/**
+ * init_dog - writes the character c to stdout
+ *
+ * @d: variable to initialize
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
