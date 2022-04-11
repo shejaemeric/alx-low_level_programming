11b07b94c58db719eb9b6fbb22d295b546087d25
shejaemeric <shejaemeric>
2022-04-11 18:39:11 +0000

    updates😉

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog structure 😂
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_H_ */
