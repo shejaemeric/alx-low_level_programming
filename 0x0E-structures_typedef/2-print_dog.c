84d24a4d58251a4a94a52e7436f1242c6a6a9fcf
shejaemeric <shejaemeric>
2022-04-11 19:46:34 +0000

    updates

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,24 @@
-void print_dog(struct dog *d);
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - writes the character c to stdout
+ *
+ * @d: variable to initialize
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
