30be2bcd1ab226c52792ac85ba06f3fa08f25fc7
shejaemeric <shejaemeric>
2022-04-13 10:19:26 +0000

    array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
