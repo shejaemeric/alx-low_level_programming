35cd1f6354a04aecf55c4026f3db92609bdc379a
shejaemeric <shejaemeric>
2022-04-13 20:00:20 +0000

    updates

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	while (i < size)
-	{
-		action(array[i]);
-		i++;
-	}
+	if (array != 0 && size != 0 && action != 0)
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 }
