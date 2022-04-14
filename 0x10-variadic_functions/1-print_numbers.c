c0aeda7dfd82d399ba251fe780cb13a8912bf328
shejaemeric <shejaemeric>
2022-04-14 11:58:47 +0000

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,13 +14,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 0; i < n - 1; i++)
+	if (separator != NULL)
 	{
-		printf("%d", va_arg(ap, int));
-		if (separator != NULL)
-			printf("%s", separator);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d", va_arg(ap, int));
+		}
 	}
-
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			printf("%s", separator);
+		}
+	}
 	printf("%d\n", va_arg(ap, int));
 	va_end(ap);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * print_strings - print strings
+ * print_numbers - print numbers
  *
  * @separator: 1st arg
  * @n: 2nd arg
@@ -11,17 +11,13 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	char *s1;
-	char *s2;
 	unsigned int i;
 	char *s = NULL;
 
 	va_start(ap, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		s1 = (va_arg(ap, char *) != s) ? va_arg(ap, char *) : "(nil)";
-		s2 = (*separator != '\0') ? separator : "";
-		printf("%s%s", s1, s2);
+		printf("%s%s", (va_arg(ap, char *) != s) ? va_arg(ap, char *) : "(nil)", (*separator != '\0') ? separator : "");
 	}
 
 	printf("%s\n", va_arg(ap, char *));
